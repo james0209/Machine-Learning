f9103f5267b1646d543e5f2cc331fc3082805314
James Brookes <jpebrookes@gmail.com>
2021-03-25 05:17:28 +0000

    re-upload AttributesMeasure

---
+++ src/main/java/ml_6002b_coursework/AttributeMeasures.java
@@ -0,0 +1,28 @@
+package ml_6002b_coursework;
+
+public class AttributeMeasures {
+
+    public static double measureInformationGain(int[][] array){
+        return 0.0;
+    }
+
+    public static double measureGini(int[][] array){
+        return 0.0;
+    }
+
+    public static double measureChiSquared(int[][] array){
+        return 0.0;
+    }
+
+    public static double measureChiSquaredYates(int[][] array){
+        return 0.0;
+    }
+
+    public String toString() {
+        return "measure " + "insert here " + "for headache splitting diagnosis = " + "insert here";
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
